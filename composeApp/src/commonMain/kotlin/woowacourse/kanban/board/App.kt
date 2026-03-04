a3491e12764f0185b69310ab00c286113bec3db8
Redish03 <pluto0303@pukyong.ac.kr>
2026-03-04 15:27:09 +0900

    feat(App.kt): 프로필에 아이콘, 담당자 UI 구현

--- composeApp/src/commonMain/kotlin/woowacourse/kanban/board/App.kt
+++ composeApp/src/commonMain/kotlin/woowacourse/kanban/board/App.kt
@@ -7,13 +7,16 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.safeContentPadding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.material.icons.rounded.AccountBox
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
@@ -38,6 +41,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import kanbanboard.composeapp.generated.resources.Res
 import kanbanboard.composeapp.generated.resources.compose_multiplatform
+import kanbanboard.composeapp.generated.resources.profile
 import org.jetbrains.compose.resources.painterResource
 
 @Composable
@@ -47,7 +51,8 @@ fun App() {
         title = "LazyColumn 컴포넌트 구현",
         script = "세로 스크롤 가능한 리스트 컴포넌트를 만들고 성능 최적화를 적용합니다.",
         tags = listOf("컴포넌트", "성능"),
-        nickname = "다이노")
+        nickname = "다이노",
+    )
 
 }
 
@@ -56,15 +61,16 @@ fun TaskCard(
     title: String,
     script: String?,
     tags: List<String>?,
-    nickname: String) {
+    nickname: String,
+) {
     Card(
         modifier = Modifier
             .width(286.dp),
         colors = CardDefaults.cardColors(
-            containerColor = Color.White
+            containerColor = Color.White,
         ),
         border = BorderStroke(1.dp, Color(0xffE5E7EB)),
-        shape = RoundedCornerShape(10.dp)
+        shape = RoundedCornerShape(10.dp),
     ) {
         Column {
             Text(
@@ -73,18 +79,31 @@ fun TaskCard(
                 fontWeight = FontWeight.Bold,
                 color = Color(0xFF101828),
                 maxLines = 1,
-                overflow = TextOverflow.Ellipsis
+                overflow = TextOverflow.Ellipsis,
             )
-            if(script != null) Text(
+            if (script != null) Text(
                 text = script,
                 fontSize = 14.sp,
                 fontWeight = FontWeight.Normal,
                 color = Color(0xff4A5565),
                 maxLines = 2,
-                overflow = TextOverflow.Ellipsis
+                overflow = TextOverflow.Ellipsis,
             )
-            if(tags != null) // TODO : TAG 구현
+            if (tags != null) // TODO : TAG 구현
             HorizontalDivider()
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Image(
+                    painter = painterResource(Res.drawable.profile),
+                    contentDescription = "프로필 이미지",
+                )
+                Spacer(modifier = Modifier.width(8.dp))
+                Text(
+                    text = nickname,
+                    fontSize = 14.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = Color(0xff364153),
+                )
+            }
         }
     }
 }
