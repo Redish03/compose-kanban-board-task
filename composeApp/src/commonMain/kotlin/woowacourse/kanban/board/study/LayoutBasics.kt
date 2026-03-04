844f4ad9ad9da6d1861f61143100fa384e90b3c6
Redish03 <pluto0303@pukyong.ac.kr>
2026-03-04 14:03:59 +0900

    feat: LayoutExample 구현

--- composeApp/src/commonMain/kotlin/woowacourse/kanban/board/study/LayoutBasics.kt
+++ composeApp/src/commonMain/kotlin/woowacourse/kanban/board/study/LayoutBasics.kt
@@ -1,16 +1,24 @@
 package woowacourse.kanban.board.study
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.key.Key.Companion.R
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.FontScaling
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import kanbanboard.composeapp.generated.resources.Res
 import kanbanboard.composeapp.generated.resources.compose_multiplatform
@@ -39,9 +47,11 @@ fun ImageExample() {
 
 @Composable
 fun ButtonExample() {
-    Button(onClick = {
-        println("버튼 클릭!")
-    }) {
+    Button(
+        onClick = {
+            println("버튼 클릭!")
+        },
+    ) {
         Text(text = "저장")
     }
 }
@@ -49,12 +59,104 @@ fun ButtonExample() {
 @Composable
 @Preview
 fun ButtonExample2() {
-    Button(onClick = {
-        println("버튼 클릭!")
-    }) {
+    Button(
+        onClick = {
+            println("버튼 클릭!")
+        },
+    ) {
         Image(
             painter = painterResource(Res.drawable.woowacourse),
             contentDescription = "버튼 이미지",
+        )
+    }
+}
+
+
+@Composable
+@Preview
+fun ColumnExample() {
+    Column {
+        Text("첫 번째 아이템")
+        Text("두 번째 아이템")
+        Text("세 번째 아이템")
+    }
+}
+
+@Composable
+@Preview
+fun RowExample() {
+    Row {
+        Text("첫 번째 아이템")
+        Text("두 번째 아이템")
+        Text("세 번째 아이템")
+    }
+}
+
+@Composable
+@Preview
+fun HeartIconExample() {
+    Button(
+        onClick = {
+            print("click")
+        },
+    ) {
+        Row() {
+            Image(
+                painter = painterResource(Res.drawable.woowacourse),
+                contentDescription = "버튼 & 이미지 & 텍스트",
+            )
+            Text("좋아요")
+        }
+    }
+}
+
+@Composable
+@Preview
+fun BoxExample() {
+    Box(
+        modifier = Modifier.size(120.dp),
+    ) {
+        Box(
+            modifier = Modifier
+                .size(100.dp)
+                .background(Color.Blue)
+                .align(Alignment.TopStart)
+        )
+
+        Box(
+            modifier = Modifier
+                .size(100.dp)
+                .background(Color.Green)
+                .align(Alignment.BottomEnd)
+        )
+    }
+}
+
+@Composable
+@Preview
+fun ColorBoxExample() {
+    Box(
+        modifier = Modifier.size(200.dp),
+    ) {
+        Box(
+            modifier = Modifier
+                .size(50.dp)
+                .background(Color.Red)
+                .align(Alignment.TopStart)
+        )
+
+        Box(
+            modifier = Modifier
+                .size(50.dp)
+                .background(Color.Yellow)
+                .align(Alignment.Center)
+        )
+
+        Box(
+            modifier = Modifier
+                .size(50.dp)
+                .background(Color.Green)
+                .align(Alignment.BottomEnd)
         )
     }
 }
