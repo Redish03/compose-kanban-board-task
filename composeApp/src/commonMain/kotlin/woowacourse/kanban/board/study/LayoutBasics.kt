9b7745757d24555fe32e9848690212484a48e5fc
Redish03 <pluto0303@pukyong.ac.kr>
2026-03-04 13:29:07 +0900

    feat: ImageExample 구현

--- composeApp/src/commonMain/kotlin/woowacourse/kanban/board/study/LayoutBasics.kt
+++ composeApp/src/commonMain/kotlin/woowacourse/kanban/board/study/LayoutBasics.kt
@@ -1,16 +1,21 @@
 package woowacourse.kanban.board.study
 
+import androidx.compose.foundation.Image
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.key.Key.Companion.R
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.FontScaling
 import androidx.compose.ui.unit.sp
+import kanbanboard.composeapp.generated.resources.Res
+import kanbanboard.composeapp.generated.resources.compose_multiplatform
+import kanbanboard.composeapp.generated.resources.woowacourse
+import org.jetbrains.compose.resources.painterResource
 
 @Composable
-@Preview
 fun TextExample() {
     Text(text = "Jetpack Compose Text 실습",
         color = Color.Blue,
@@ -19,6 +24,11 @@ fun TextExample() {
         fontWeight = FontWeight.Bold)
 }
 
+@Composable
+@Preview
 fun ImageExample() {
-
+    Image(
+        painter = painterResource(Res.drawable.woowacourse),
+        contentDescription = "설명 텍스트 (접근성을 위해 중요!)"
+    )
 }
