7c1e718c475d37edc45f5b762e9f4f8ada8512fc
Redish03 <pluto0303@pukyong.ac.kr>
2026-03-04 16:01:37 +0900

    feat(App.kt): 구분선 구현

--- composeApp/src/commonMain/kotlin/woowacourse/kanban/board/App.kt
+++ composeApp/src/commonMain/kotlin/woowacourse/kanban/board/App.kt
@@ -4,6 +4,7 @@ import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -121,7 +122,10 @@ fun TaskCard(
                     }
                 }
             }
-            HorizontalDivider()
+            HorizontalDivider(
+                thickness = 1.dp,
+                color = Color(0xFFF3F4F6)
+            )
             Row(verticalAlignment = Alignment.CenterVertically) {
                 Image(
                     painter = painterResource(Res.drawable.profile),
