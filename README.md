6e998820fea858c1da4e1bde0f69ffc782b351d4
Redish03 <pluto0303@pukyong.ac.kr>
2026-03-04 14:39:03 +0900

    feat(App.kt): 카드 구현

--- README.md
+++ README.md
@@ -2,9 +2,9 @@
 
 # 기능 구현 사항
 
-- [ ] Card 구현
-    - [ ] 배경색 설정
-    - [ ] Card 테두리 색상, radius 설정
+- [x] Card 구현
+    - [x] 배경색 설정
+    - [x] Card 테두리 색상, radius 설정
 - [ ] title 구현
     - [ ] maxLine 1줄로 제한, 말줄임표 구현
 - [ ] script 구현
--- composeApp/src/commonMain/kotlin/woowacourse/kanban/board/App.kt
+++ composeApp/src/commonMain/kotlin/woowacourse/kanban/board/App.kt
@@ -1,12 +1,20 @@
 package woowacourse.kanban.board
 
 import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.safeContentPadding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -16,29 +24,59 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import kanbanboard.composeapp.generated.resources.Res
 import kanbanboard.composeapp.generated.resources.compose_multiplatform
 import org.jetbrains.compose.resources.painterResource
 
 @Composable
-@Preview
+@Preview(showBackground = true)
 fun App() {
-    MaterialTheme {
-        var showContent by remember { mutableStateOf(false) }
-        Column(
-            modifier = Modifier
-                .background(MaterialTheme.colorScheme.primaryContainer)
-                .safeContentPadding()
-                .fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally,
-        ) {
-            Button(onClick = { showContent = !showContent }) {
-                Text("Click me!")
-            }
-            AnimatedVisibility(showContent) {
-                Image(painterResource(Res.drawable.compose_multiplatform), null)
-            }
+
+}
+
+@Composable
+@Preview
+fun TaskCard() {
+    Card(
+        modifier = Modifier
+            .width(286.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = Color.White
+        ),
+        border = BorderStroke(1.dp, Color(0xffE5E7EB)),
+        shape = RoundedCornerShape(10.dp)
+    ) {
+        Column {
+            Text("title")
+            Text("script")
+            HorizontalDivider()
+
         }
     }
 }
+
+
+//
+//@Composable
+//fun CheckerScreen() {
+//    var checked by remember { mutableStateOf(true) }
+//
+//    CheckerView(checked = checked) {
+//        checked = !checked
+//    }
+//}
+//
+//@Composable
+//fun CheckerView(checked: Boolean, check: () -> Unit) {
+//    Column {
+//        Checkbox(
+//            checked = checked,
+//            onCheckedChange = { check() },
+//        )
+//        if(checked) Text(text = "체크됨!")
+//    }
+//}
