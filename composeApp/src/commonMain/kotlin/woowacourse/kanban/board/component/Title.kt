245fc401c304dac94ff2af07cfe778bea691bbbc
Redish03 <pluto0303@pukyong.ac.kr>
2026-03-04 16:55:29 +0900

    refactor: 불필요한 라이브러리 제거

--- composeApp/src/commonMain/kotlin/woowacourse/kanban/board/component/Title.kt
+++ composeApp/src/commonMain/kotlin/woowacourse/kanban/board/component/Title.kt
@@ -2,7 +2,6 @@ package woowacourse.kanban.board.component
 
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.sp
