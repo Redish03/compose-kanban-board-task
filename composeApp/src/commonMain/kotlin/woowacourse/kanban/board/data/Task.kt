f04ed1825826a95ef0ec3f5210a0607cbc20101f
Redish03 <pluto0303@pukyong.ac.kr>
2026-03-09 13:49:15 +0900

    refactor: Task 프로퍼티 변수 명 변경

--- composeApp/src/commonMain/kotlin/woowacourse/kanban/board/data/Task.kt
+++ composeApp/src/commonMain/kotlin/woowacourse/kanban/board/data/Task.kt
@@ -4,10 +4,10 @@ data class Task(
     val taskTitle: String,
     val taskScript: String = "",
     val tags: List<String> = emptyList(),
-    val profileName: String,
+    val nickname: String,
 ) {
     init {
         require(taskTitle.isNotBlank()) { "할 일의 제목은 빈칸이거나 공백 일 수 없습니다." }
-        require(profileName.isNotBlank()) { "사용자 이름은 빈칸이거나 공백 일 수 없습니다." }
+        require(nickname.isNotBlank()) { "사용자 이름은 빈칸이거나 공백 일 수 없습니다." }
     }
 }
