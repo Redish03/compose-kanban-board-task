1a26e7d9b50e850fad5e498267027d96d258c16d
Redish03 <pluto0303@pukyong.ac.kr>
2026-03-09 13:47:07 +0900

    feat(Task.kt): 할 일 제목과 이름은 빈칸 일 수 없는 규칙 추가

--- composeApp/src/commonMain/kotlin/woowacourse/kanban/board/data/Task.kt
+++ composeApp/src/commonMain/kotlin/woowacourse/kanban/board/data/Task.kt
@@ -5,4 +5,9 @@ data class Task(
     val taskScript: String = "",
     val tags: List<String> = emptyList(),
     val profileName: String,
-)
+) {
+    init {
+        require(taskTitle.isNotBlank()) { "할 일의 제목은 빈칸이거나 공백 일 수 없습니다." }
+        require(profileName.isNotBlank()) { "사용자 이름은 빈칸이거나 공백 일 수 없습니다." }
+    }
+}
