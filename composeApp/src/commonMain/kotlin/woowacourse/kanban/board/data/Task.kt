e7af700bb2feb6e30eda80191c54ee0426feca03
Redish03 <pluto0303@pukyong.ac.kr>
2026-03-09 10:53:26 +0900

    refactor(Task.kt): 디폴트 파라미터 넣음

--- composeApp/src/commonMain/kotlin/woowacourse/kanban/board/data/Task.kt
+++ composeApp/src/commonMain/kotlin/woowacourse/kanban/board/data/Task.kt
@@ -2,6 +2,7 @@ package woowacourse.kanban.board.data
 
 data class Task(
     val taskTitle: String,
-    val taskScript: String?,
-    val tags: List<String>?, val profileName: String,
+    val taskScript: String = "",
+    val tags: List<String> = emptyList(),
+    val profileName: String,
 )
